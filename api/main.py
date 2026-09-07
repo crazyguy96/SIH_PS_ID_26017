c0e646012ec17ec93b97b011ad43ae2205a664b7
shaurya jain <shauryajain0410@gmail.com>
2026-09-07 11:58:53 +0530

    Allow Netlify frontend in FastAPI CORS

--- api/main.py
+++ api/main.py
@@ -20,6 +20,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        "https://luminous-melomakarona-8b211d.netlify.app",
+
     ],
     allow_credentials=True,
     allow_methods=["*"],
